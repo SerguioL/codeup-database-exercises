6130d981df9a9db7c36b1eeb5b7b60f45e14ffe5
SerguioL <serguio.l.landa@gmail.com>
2021-10-05 08:52:34 -0500

    finished

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,25 @@
 USE employees;
 
 SELECT first_name FROM employees
-WHERE first_name IN('Irena', 'Vidya','Maya');
+WHERE gender ='M'
+AND ( first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya');
 
 SELECT last_name FROM employees
 WHERE last_name LIKE('e%');
 
 SELECT last_name FROM employees
+WHERE last_name LIKE('E%')
+OR last_name LIKE ('%E');
+
+SELECT last_name FROM employees
+WHERE last_name LIKE('E%')
+   AND last_name LIKE ('%E');
+
+SELECT last_name FROM employees
 WHERE last_name LIKE('%q%');
+
+SELECT last_name FROM employees
+WHERE last_name LIKE('%q%')
+AND last_name NOT LIKE '%qu%';
